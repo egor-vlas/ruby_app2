7bbb14e6e5f0810c1b3aff1830802d38972045c0
egor-vlas <vlasenkoiegor2021@gmail.com>
2021-05-06 08:33:09 +1000

    destroy functionality added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,38 @@
 class TasksController < ApplicationController
   def index
-  	@tasks = []
+  	@tasks = Task.all 
   end
 
   def new
+  	p @task = Task.new
+  end
+
+  def create
+  	# new will reacieve a parametr from the virew form
+  	@task = Task.new(task_params)
+  	@task.save
+  	redirect_to tasks_path
+  end
+
+  def edit
+  	@task = Task.find(params[:id])
+  end
+
+  def update
+  	@task = Task.find(params[:id])
+  	@task.update(task_params)
+  	redirect_to tasks_path
+  end
+
+  def destroy
+  	@task = Task.find(params[:id])
+  	@task.destroy
+  	redirect_to tasks_path
+  end
+
+  private
+  #whitelist parameters
+  def task_params
+  	params.require(:task).permit(:title, :details)
   end
 end
